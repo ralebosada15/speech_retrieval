6491415d75ab80975cd9d611ecc65ef8277333be
Ravtor <ralebosada@up.edu.ph>
2026-05-13 18:11:10 +0800

    Revised SNR formula

--- process/snr.m
+++ process/snr.m
@@ -5,7 +5,7 @@
 function [snr_db, signal_power, noisy_power] = snr(clean_speech, process_speech)
     % Computation of Power of Clean Signal and Noisy Signal
     signal_power = mean(clean_speech.^2);
-    noisy_power = mean(process_speech.^2);
+    noisy_power = mean((clean_speech - process_speech).^2);
 
     snr_db = 10*log10(signal_power/noisy_power);
 end
